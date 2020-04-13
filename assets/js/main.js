45d2509acfdf18d9d573f2b326a7b7ef71a6f4a7
Higor M. G. Ferreira <moroni.higor@gmail.com>
2020-04-12 22:41:29 -0300

    Parei por hoje
    implementando as funcionalidades das dicas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,27 +22,34 @@ let dom = {
         this.btnDesistir.style.backgroundColor = '#242526'
     },
     testarDicas() {
-        alert(this.casas.casa1[1].value)
-        //Dica 0
+        //Dica 0 - O Norueguês vive na primeira casa.
         if (this.casas.casa1[1].value == 3) {
             this.dicas[0].setAttribute('checked', 'true')
         } else {
             this.dicas[0].removeAttribute('checked', 'true')
         }
-            //Dica 1
-            //Dica 2
-            //Dica 3
-            //Dica 4
-            //Dica 5
-            //Dica 6
-            //Dica 7
-            //Dica 8
-            //Dica 9
-            //Dica 10
-            //Dica 11
-            //Dica 12
-            //Dica 13
-            //Dica 14
+        //Dica 1 - O Inglês vive na casa Vermelha.
+        for (let i in this.casas) {
+            console.log(this.casas.i)
+            if (this.casas.i[1].value == 2 && this.casas.i[0].value == 4) {
+                this.dicas[1].setAttribute('checked', 'true')
+            } else {
+            this.dicas[1].removeAttribute('checked', 'true')
+            }
+        }
+        //Dica 2 - O Sueco tem Cachorros como animais de estimação.
+        //Dica 3 - O Dinamarquês bebe Chá.
+        //Dica 4 - A casa Verde fica do lado esquerdo da casa Branca.
+        //Dica 5 - O homem que vive na casa Verde bebe Café.
+        //Dica 6 - O homem que fuma Pall Mall cria Pássaros.
+        //Dica 7 - O homem que vive na casa Amarela fuma Dunhill.
+        //Dica 8 - O homem que vive na casa do meio bebe Leite.
+        //Dica 9 - O homem que fuma Blends vive ao lado do que tem Gatos.
+        //Dica 10 - O homem que cria Cavalos vive ao lado do que fuma Dunhill.
+        //Dica 11 - O homem que fuma BlueMaster bebe Cerveja.
+        //Dica 12 - O Alemão fuma Prince.
+        //Dica 13 - O Norueguês vive ao lado da casa Azul.
+        //Dica 14 - O homem que fuma Blends é vizinho do que bebe Água.
     }
 }
 function desistir() {
